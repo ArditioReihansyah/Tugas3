8e749a5cc22bb79e5de0109fa45779c6c0456259
ArditioReihansyah <arditio.reihansyah11@ui.ac.id>
2022-09-14 20:31:18 +0700

    Penyelesaian Tugas1

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_catalogitem
+
+app_name = 'Katalog'
+
+urlpatterns = [
+    path('', show_catalogitem, name='show_catalogitem'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_catalogitem(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_catalog_item': data_catalog_item,
+        'nama': 'Arditio Reihansyah Putra Pradana',
+        'npm' : '2106751972'
+    }
+    return render(request,"katalog.html",context)
